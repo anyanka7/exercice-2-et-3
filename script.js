2a10c25dd6bb501eef4ae8f593bfdae18e5007d6
anyanka7 <57446871+anyanka7@users.noreply.github.com>
2019-11-25 12:56:52 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function division() {
+    document.getElementById('photo').style.width='350px';
+}
+function displayKey() {
+    var keyType = document.getElementById('alert').value;
+    alert(keyType);
+}
